f3477bfa39cd1b61bc19c33fce7510177522f7cc
Сергѣй Батанов <dmpas@dmpas.ru>
2016-06-06 22:10:17 +0300

    Первое описание создания компонента.

---
+++ README.md
@@ -0,0 +1,18 @@
+1. Создаём новый проект-библиотеку
+2. Подключаем NuGet пакет OneScript runtime core
+3. Подключаем модули:
+```
+using ScriptEngine.Machine.Contexts;
+using using ScriptEngine.Machine;
+```
+
+4. Ставим на класс пометку `[ContextClass("МойКласс", "MyClass")]` и добавляем классу наследование от `AutoContext<MyClass>`
+5. Прописываем в класс конструктор
+```
+[ScriptConstructor]
+public static IRuntimeContextInstance Constructor()
+{
+    return new MyClass();
+}
+```
+
---
+++ oscript-component/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.Machine;
+
+namespace oscriptcomponent
+{
+    [ContextClass("МойКласс", "MyClass")]
+    public class MyClass : AutoContext<MyClass>
+    {
+        public MyClass()
+        {
+        }
+
+        [ContextProperty("СвойствоДляЧтения", "ReadonlyProperty")]
+        public string ReadonlyProperty {
+            get {
+                return "MyValue";
+            }
+        }
+            
+        [ScriptConstructor]
+        public static IRuntimeContextInstance Constructor()
+        {
+            return new MyClass();
+        }
+    }
+}
+
