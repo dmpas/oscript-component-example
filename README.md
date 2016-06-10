8e0e912bfb9f06588f4428e6b44901d9fd742c14
Сергѣй Батанов <dmpas@dmpas.ru>
2016-06-10 15:03:36 +0300

    Расширены примеры разработки компоненты.
    * Методы
    * Несколько классов
    * Пример нескольких конструкторов
    * Работа с внутренними значениями IValue
    * Использование встроенных классов

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Как создать компонент для Односкрипта
 
 1.  Создаём новый проект-библиотеку
-2.  Подключаем NuGet пакет OneScript runtime core
+2.  Подключаем NuGet пакет "OneScript runtime core" и "OneScript Main Client Libraries".
+    Первый подключать обязательно, второй подключается для возможности использования
+    встроенных типов Массив, ТаблицаЗначений и т.д.
 3.  Подключаем модули:
         
         using ScriptEngine.Machine.Contexts;
         using ScriptEngine.Machine;
+        using ScriptEngine.HostedScript.Library; // только если подключили OneScript Main Client Libraries
         
 4.  Ставим на класс пометку `[ContextClass("МойКласс", "MyClass")]` и добавляем классу наследование от `AutoContext<MyClass>`
 5.  Прописываем в класс конструктор
@@ -15,3 +18,10 @@
         {
             return new MyClass();
         }
+
+6.  После чего в коде можно использовать вызов вида
+
+        ПодключитьВнешнююКомпоненту("oscript-component/bin/Debug/oscript-component.dll");
+        ОбъектМоегоКласса = Новый МойКласс;
+
+
---
+++ oscript-component/Addition.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.Machine;
+using ScriptEngine.HostedScript.Library;
+
+namespace oscriptcomponent
+{
+    [ContextClass("Сложение", "Addition")]
+    public class Addition : AutoContext<Addition>
+    {
+        // Возьмём Массив из стандартной библиотеки
+        private ArrayImpl items;
+
+        public Addition()
+        {
+            items = ArrayImpl.Constructor() as ArrayImpl;
+
+            // Также можно написать items = new ArrayIml();
+            // но лучше вызвать предлагаемый библиотекой конструктор
+        }
+
+        // Здесь объявляется свойство только для чтения Слагаемые
+        // Несмотря на то, что само свойство только для чтения, содержимое массива
+        // можно изменять из кода:
+        //     Сложение.Слагаемые.Добавить("Что угодно");
+
+        [ContextProperty("Слагаемые", "Items")]
+        public ArrayImpl Items 
+        {
+            get  { return items; }
+        }
+
+        [ContextMethod("ДобавитьСлагаемое", "AddItem")]
+        public void AddItem(IValue item)
+        {
+            var itemToAdd = SumItem.Constructor(item);
+            items.Add(ValueFactory.Create(itemToAdd));
+        }
+
+        [ContextMethod("Вычислить", "Calculate")]
+        public IValue Calculate()
+        {
+            Decimal result = 0;
+            foreach (var item in items)
+            {
+                var sumItem = item as SumItem;
+                result += sumItem.Value;
+            }
+            return ValueFactory.Create(result);
+        }
+
+        // Пример нескольких конструкторов
+
+        [ScriptConstructor]
+        public static IRuntimeContextInstance Constructor()
+        {
+            return new Addition();
+        }
+
+        [ScriptConstructor]
+        public static IRuntimeContextInstance Constructor(IValue item1, IValue item2)
+        {
+            var addition = new Addition();
+            addition.AddItem(item1);
+            addition.AddItem(item2);
+            return addition;
+        }
+
+        [ScriptConstructor]
+        public static IRuntimeContextInstance Constructor(IValue oldAdditionParam)
+        {
+            var oldAddition = oldAdditionParam.GetRawValue() as Addition;
+            if (oldAddition == null)
+            {
+                throw RuntimeException.InvalidArgumentType();
+            }
+
+            var addition = new Addition();
+            foreach (var item in oldAddition.items)
+            {
+                addition.items.Add(item);
+            }
+
+            return addition;
+        }
+
+    }
+}
+
