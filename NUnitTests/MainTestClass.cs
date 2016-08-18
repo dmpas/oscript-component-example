643ec08301d697630d224fc09556e1f3e425351d
Сергѣй Батанов <dmpas@dmpas.ru>
2016-08-18 22:43:59 +0300

    Добавлен проект модульного тестирования

---
+++ NUnitTests/MainTestClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using ScriptEngine.HostedScript;
+using ScriptEngine.Machine;
+using ScriptEngine.Environment;
+using oscriptcomponent;
+
+// Используется NUnit 3.6
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class MainTestClass
+    {
+
+        private EngineHelpWrapper host;
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            host = new EngineHelpWrapper();
+            host.StartEngine();
+        }
+
+        [Test]
+        public void TestAsInternalObjects()
+        {
+            var item1 = new SumItem(1);
+            var item2 = new SumItem(2);
+            var sum = new Addition();
+
+            sum.AddItem(item1);
+            sum.AddItem(item2);
+
+            Assert.AreEqual(sum.Calculate(), ValueFactory.Create(3));
+
+            sum.AddItem(new SumItem(3));
+            Assert.AreEqual(sum.Calculate(), ValueFactory.Create(6));
+
+            sum.AddItem(new SumItem(-1));
+            Assert.AreEqual(sum.Calculate(), ValueFactory.Create(5));
+        }
+
+        [Test]
+        public void TestAsExternalObjects()
+        {
+            host.RunTestScript("NUnitTests.Tests.external.os");
+        }
+    }
+}
