16805167f434ab3e0ec81b2b009530ae6bb9834b
Сергѣй Батанов <dmpas@dmpas.ru>
2017-01-05 01:20:33 +0300

    Форматирование.

--- oscript-component/MyClass.cs
+++ oscript-component/MyClass.cs
@@ -4,25 +4,27 @@ using ScriptEngine.Machine;
 
 namespace oscriptcomponent
 {
-    [ContextClass("МойКласс", "MyClass")]
-    public class MyClass : AutoContext<MyClass>
-    {
-        public MyClass()
-        {
-        }
+	[ContextClass("МойКласс", "MyClass")]
+	public class MyClass : AutoContext<MyClass>
+	{
+		public MyClass()
+		{
+		}
 
-        [ContextProperty("СвойствоДляЧтения", "ReadonlyProperty")]
-        public string ReadonlyProperty {
-            get {
-                return "MyValue";
-            }
-        }
-            
-        [ScriptConstructor]
-        public static IRuntimeContextInstance Constructor()
-        {
-            return new MyClass();
-        }
-    }
+		[ContextProperty("СвойствоДляЧтения", "ReadonlyProperty")]
+		public string ReadonlyProperty
+		{
+			get
+			{
+				return "MyValue";
+			}
+		}
+
+		[ScriptConstructor]
+		public static IRuntimeContextInstance Constructor()
+		{
+			return new MyClass();
+		}
+	}
 }
 
